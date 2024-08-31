4019f01f705c0722916a7a32e0f5ea1b1ede74f8
seokjin <tjrwls08088@naver.com>
2024-08-31 17:30:52 +0900

    2024_08_31

--- src/week_6/harvartz/PGS_네트워크.java
+++ src/week_6/harvartz/PGS_네트워크.java
@@ -2,41 +2,30 @@ package week_6.harvartz;
 
 public class PGS_네트워크 {
     int count = 0;
-    int[][] network;
-    // 노드에 방문했는지 안했는지 확인하는 배열
-    boolean[] visited;
+    boolean[] visited; // 노드에 방문했는지 안했는지 확인하는 배열
+
     public int solution(int n, int[][] computers) {
         visited = new boolean[n];
 
-        for(int i = 0; i<n; i++){
-            if(visited[i]) continue;
-            visited[i] = true;
-            dfs(i, n, computers);
-            if(isComplete(visited)) {
-                count++;
-                return count;
-            }
-            if(!isComplete(visited)){
+        for (int i = 0; i < n; i++) {
+            // 굳이 함수를 만들어서 모두 방문하지 않은 노드를 확인할 필요가 없다
+            // 해당 for문 자체가 그 일을 하기 때문
+            if (!visited[i]) {
+                visited[i] = true;
+                dfs(i, n, computers);
                 count++;
             }
         }
         return count;
     }
 
-    public void dfs(int start, int size, int[][] computers){
-        for(int i = 0; i<size; i++){
+    public void dfs(int start, int size, int[][] computers) {
+        for (int i = 0; i < size; i++) {
             // 연결되어있는지 && 다음 탐색하려는 노드가 방문안했다면
-            if(computers[start][i] == 1 && !visited[i]){
+            if (computers[start][i] == 1 && !visited[i]) {
                 visited[i] = true;
                 dfs(i, size, computers);
             }
         }
-    }
-
-    public boolean isComplete(boolean[] visited){
-        for(boolean i : visited){
-            if(!i) return false;
-        }
-        return true;
     }
 }
