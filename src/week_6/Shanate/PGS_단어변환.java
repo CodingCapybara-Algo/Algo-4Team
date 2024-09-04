31601ec7889d03a67b45b257ecdaac2929ac23ab
Shanate <redeyes5240@gmail.com>
2024-09-04 11:28:35 +0900

    2024_09_03

--- src/week_6/Shanate/PGS_단어변환.java
+++ src/week_6/Shanate/PGS_단어변환.java
@@ -3,6 +3,13 @@ package week_6.Shanate;
 import java.io.*;
 import java.util.*;
 
+/*
+처음 생각했을 때,
+{"hot", "dot", "dog", "lot", "log", "cog"} 이걸 순서대로 가는거라 생각.
+그게 아닌, 현재 단어에서 한 알파벳만 제외하고 일치하는 것을 찾아서 가는 것이 목표.
+hit에서 hot을 가는 이유는 순서가 그래서 그런 것이 아닌, o 하나만 다르기에 이를 바꿀 수 있어서다.
+*/
+
 public class PGS_단어변환 {
     static int answer;
     static boolean[] visited;
@@ -18,7 +25,7 @@ public class PGS_단어변환 {
     }
 
     static void dfs(String cur, String target, String[] words, int depth) {
-        if(cur.equals(target)) { // 종료
+        if(cur.equals(target)) { // 현재 단어와 타겟 단어 일치 시 종료
             answer = depth;
             return;
         }
@@ -27,18 +34,18 @@ public class PGS_단어변환 {
             if(visited[i]) { // 중복 방문 제거 처리
                 continue;
             }
-            int k = 0;
-            for(int j=0; j<cur.length(); j++){ // 현재 단어와 문자열 비교
+            int k = 0; // 현재 단어와 찾은 단어 간의 문자 수를 카운팅
+            for(int j=0; j<cur.length(); j++){ // 현재 단어와 찾은 문자열 비교
                 if(cur.charAt(j) == words[i].charAt(j)) {
                     k++;
                 }
             }
-            if(k==cur.length()-1) { // 일치한 문자열 갯수 = 현재 단어에서 한 글자 뺀 경우의 갯수
-                visited[i] = true;
-                dfs(words[i], target, words, depth + 1);
-                visited[i] = false;
+            if(k==cur.length()-1) { // 일치한 문자열 갯수 = 현재 단어에서 한 글자 뺀 경우의 갯수 -> 한 글자만 다를 경우
+                visited[i] = true; // 방문 처리
+                dfs(words[i], target, words, depth + 1); // 카운팅 증가하고 다음 단어를 찾으러 재귀 호출
+                visited[i] = false; // 백트래킹
             }
-        }
+        } // if 문 조건 불만족 시, for 루프 재실행하여 그 다음 문자열 탐색
     }
 
     public static void main(String[] args) {
